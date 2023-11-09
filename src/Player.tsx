aa08857b96d81fc4cb8193406d26fc1c8cf2b7a4
Curetix <curetix@mailbox.org>
2023-11-09 15:13:12 +0000

    Update 10 files

--- src/Player.tsx
+++ src/Player.tsx
@@ -1,13 +1,29 @@
-import '@vidstack/react/player/styles/default/theme.css';
-import '@vidstack/react/player/styles/default/layouts/video.css';
-import { MediaPlayer, MediaProvider } from '@vidstack/react';
-import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
+import "@vidstack/react/player/styles/default/theme.css";
+import "@vidstack/react/player/styles/default/layouts/video.css";
+import { MediaPlayer, MediaProvider, Poster } from "@vidstack/react";
+import {
+  defaultLayoutIcons,
+  DefaultVideoLayout,
+} from "@vidstack/react/player/layouts/default";
 
 export default function Player() {
-    return (
-        <MediaPlayer title="Sprite Fight" src="https://stream.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/low.mp4" style={{width: 1280, height: 720}} autoplay>
-            <MediaProvider />
-            <DefaultVideoLayout thumbnails="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/storyboard.vtt" icons={defaultLayoutIcons} />
-        </MediaPlayer>
-    );
+  return (
+    <MediaPlayer
+      src="https://media-files.vidstack.io/1080p.mp4"
+      style={{ width: 640, height: 360 }}
+    >
+      <MediaProvider>
+        <Poster
+          className="vds-poster"
+          src="https://media-files.vidstack.io/poster.png"
+          alt="Thumbnail"
+        />
+      </MediaProvider>
+      <DefaultVideoLayout
+        smallLayoutWhen={false}
+        thumbnails="https://media-files.vidstack.io/thumbnails.vtt"
+        icons={defaultLayoutIcons}
+      />
+    </MediaPlayer>
+  );
 }
